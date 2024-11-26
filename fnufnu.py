2d5535bb365f11bbeed97255c4150e4e15f33e24
fatihtekik <123174607+fatihtekik@users.noreply.github.com>
2024-11-26 23:36:47 +0600

    Скрыл токен

--- fnufnu.py
+++ fnufnu.py
@@ -2,7 +2,7 @@ import telebot
 from telebot import types
 import psycopg2
 
-token='7576683979:AAF3OwSTLwIA_AAnu0ZVOZUpCk3lIJWsQTQ'
+token='бугага нету токена'
 bot=telebot.TeleBot(token)
 
 login=""
